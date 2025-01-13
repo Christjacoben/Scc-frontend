c6bae8d89bc9e08a8fef47b40d056f821dd62bac
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-01-13 17:07:53 +0800

    Update eventDetailsSlice.js

--- src/store/eventDetailsSlice.js
+++ src/store/eventDetailsSlice.js
@@ -5,7 +5,7 @@ export const fetchEventDetails = createAsyncThunk(
   async (eventTitle, thunkAPI) => {
     try {
       const response = await fetch(
-        `http://localhost:5000/api/scanEvent/${eventTitle}`
+        `https://ssc-backend.onrender.com/api/scanEvent/${eventTitle}`
       );
       if (!response.ok) {
         const errorData = await response.json();
