e8f1136eca31230194341c32d50e4d8caccc712b
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-01-17 17:25:48 +0800

    Create Restrict.jsx

---
+++ src/components/Restrict.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Restric.css";
+
+function Restric() {
+  return (
+    <div className="restriction-main-com">
+      <div className="restriction-con">
+        <p>Website Restricted By Developer</p>
+        <p>Due To Incomplete Payments Of</p>
+        <p>Development</p>
+      </div>
+    </div>
+  );
+}
+
+export default Restric;
